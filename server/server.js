b7308434d3eaa4ba76632fbcfadd249bf346472e
arshdhillon8560 <arshdhillon8560@gmail.com>
2025-06-26 23:52:27 +0530

    api change

--- server/server.js
+++ server/server.js
@@ -2,23 +2,29 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { inngest, functions } from './inngest/index.js';
+import { serve } from 'inngest/express';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Use IIFE to allow top-level await
+(async () => {
+  await connectDB();
 
-await connectDB();
+  app.use(express.json());
+  app.use(cors());
+  app.use(clerkMiddleware());
 
-//Middleware
-app.use (express.json());
-app.use(cors());
+  app.get('/', (req, res) => {
+    res.send('Hello from the server!');
+  });
 
+  // Inngest functions route
+  app.use('/api/inngest', serve({ client: inngest, functions }));
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+})();
