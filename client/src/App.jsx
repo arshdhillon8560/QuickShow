fb2007b6459a210fa605f8fe6546261103acbdd6
arshdhillon8560 <arshdhillon8560@gmail.com>
2025-06-28 12:08:55 +0530

    new commkyt for seatLayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+
 import Navbar from "./components/Navbar";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
-import SeatLayout from "./pages/Seatlayout";
+import SeatLayout from "./pages/SeatLayout";
 import MyBookings from "./pages/MyBookings";
 import Favorite from "./pages/Favorite";
 import { Toaster } from "react-hot-toast";
